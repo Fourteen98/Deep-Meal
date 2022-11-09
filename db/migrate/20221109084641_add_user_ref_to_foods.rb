f0336ab9c76efd118279bd7eefa5713f33d601d0
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-11-09 08:48:35 +0000

    undo and redo migration with correct migration ref syntax

--- db/migrate/20221109084641_add_user_ref_to_foods.rb
+++ db/migrate/20221109084641_add_user_ref_to_foods.rb
@@ -1,4 +1,4 @@
-class AddUserToFoods < ActiveRecord::Migration[7.0]
+class AddUserRefToFoods < ActiveRecord::Migration[7.0]
   def change
     add_reference :foods, :user, null: false, foreign_key: true
   end
