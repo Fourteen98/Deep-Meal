7688b4399a8eb3a3dae7fd31c710760d8e2a3838
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-11-10 07:40:56 +0000

    add recipe index view and action to recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,7 @@
 class RecipesController < ApplicationController
   def index
+    @user = User.find(params[:user_id])
+    @recipes = Recipe.all
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   root to: 'users#index'
   resources :users do
     resources :foods, only: [:index, :destroy, :create, :new]
+    resources :recipes, only: [:index, :destroy, :create, :new]
   end
 end
