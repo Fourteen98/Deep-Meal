51090fb3b9df39d61efc74b8c16502baa2ca33c2
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-11-11 12:16:23 +0000

    add new action for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -16,11 +16,35 @@ class RecipesController < ApplicationController
     @recipe.toggle!(:public)
   end
 
-  # private
-  #
-  # def recipe_params
-  #   params.require(:recipe).permit(:public)
-  # end
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = User.find(params[:user_id])
+    respond_to do |format|
+      if @recipe.save
+        format.html { redirect_to user_recipes_path(params[:user_id]), notice: 'Recipe was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find_by(id: params[:id])
+    @recipe.destroy
+    respond_to do |format|
+      format.html { redirect_to user_recipes_path(params[:user_id]), notice: 'Recipe was successfully destroyed.' }
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
 end
 
 # before_action :set_recipe, only: %i[show edit update destroy]
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,16 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        'brightRed': 'hsl(12, 88%, 59%)',
+        'brightRedLight': 'hsl(12, 88%, 69%)',
+        'brightRedSuperLight': 'hsl(12, 88%, 95%)',
+        'darkBlue': 'hsl(228, 39%, 23%)',
+        'veryDarkBlue': 'hsl(223, 12%, 13%)',
+        'darkGrayishBlue': 'hsl(227, 12%, 61%)',
+        'veryPaleRed': 'hsl(13, 100%, 96%)',
+        'veryLightGray': 'hsl(0, 0%, 98%)',
+      },
     },
   },
   plugins: [
