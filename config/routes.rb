c7c7703be72549163fa874b2d1f9fd6879c18a78
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-11-11 00:07:43 +0000

    add devise migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   post "users/:user_id/recipes/:id", to: "recipes#toggle"
   root to: 'users#index'
   resources :users do
