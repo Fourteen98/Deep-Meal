656d2f02a892dd5458d87d9ba091b821c5c4e1a2
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-11-12 14:14:37 +0000

    add test for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :foods, dependent: :destroy
   has_many :recipes, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true, length: { maximum: 27 }
+
 end
