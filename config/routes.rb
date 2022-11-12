da82b700e00c2f75203562092fe65447d95af3c8
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-11-12 10:33:06 +0000

    fix linters

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
     resources :foods
     resources :recipes do
       resources :recipe_foods
+      resources :shopping_list, only: [ :index ]
     end
     resources :public_recipes
   end
