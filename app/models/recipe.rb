4943709e66b9951c0ff125a0c12e715f4b77520f
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-11-12 14:17:46 +0000

    add test for recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,9 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 18 }
+  validates :preparation_time, presence: true
+  validates :cooking_time, presence: true
+  validates :description, presence: true, length: { maximum: 450 }
 end
